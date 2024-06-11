ebdab42210580a3da9b514c4f90c83665909d9c0
Josue Gonzalez <josuegr834@gmail.com>
2024-06-11 17:20:42 -0300

    Modificando el timestamp

--- src/sections/Countdown.jsx
+++ src/sections/Countdown.jsx
@@ -1,99 +1,111 @@
-import './styles/Countdown.css'
-import { useEffect } from 'react';
+import "./styles/Countdown.css";
+import { useEffect } from "react";
 
 const Countdown = () => {
-    
-    const TIMESTAMP_END = 1718294400000
-    const SECOND = 1000;
-    const MINUTE = SECOND * 60;
-    const HOUR = MINUTE * 60;
-    const DAY = HOUR * 24;
+  const TIMESTAMP_END = 1720729143000;
 
-    function init() {
-        const $countdown = document.querySelector('[data-date]');
-        if (!$countdown) return
+  const SECOND = 1000;
+  const MINUTE = SECOND * 60;
+  const HOUR = MINUTE * 60;
+  const DAY = HOUR * 24;
 
-        const $days = $countdown.querySelector('[data-days]')
-        const $hours = $countdown.querySelector('[data-hours]')
-        const $minutes = $countdown.querySelector('[data-minutes]')
-        const $seconds = $countdown.querySelector('[data-seconds]')
+  function init() {
+    const $countdown = document.querySelector("[data-date]");
+    if (!$countdown) return;
 
-        const timestamp = $countdown.getAttribute('data-date')
-        if(!timestamp) return
+    const $days = $countdown.querySelector("[data-days]");
+    const $hours = $countdown.querySelector("[data-hours]");
+    const $minutes = $countdown.querySelector("[data-minutes]");
+    const $seconds = $countdown.querySelector("[data-seconds]");
 
-        const date = new Date(+timestamp).getTime()
+    const timestamp = $countdown.getAttribute("data-date");
+    if (!timestamp) return;
 
-        const formatTime = time => {
-            return Math.floor(time).toString().padStart(2, "0")
-        }
+    const date = new Date(+timestamp).getTime();
 
-        function updateCountdown() {
-            const now = Date.now()
-            const diff = date - now
+    const formatTime = (time) => {
+      return Math.floor(time).toString().padStart(2, "0");
+    };
 
-            if ($days instanceof HTMLElement) {
-                $days.innerText = formatTime(diff / DAY)
-            }
+    function updateCountdown() {
+      const now = Date.now();
+      const diff = date - now;
 
-            if ($hours instanceof HTMLElement) {
-                $hours.innerText = formatTime((diff % DAY) / HOUR)
-            }
+      if ($days instanceof HTMLElement) {
+        $days.innerText = formatTime(diff / DAY);
+      }
 
-            if ($minutes instanceof HTMLElement) {
-                $minutes.innerText = formatTime((diff % HOUR) / MINUTE)
-            }
+      if ($hours instanceof HTMLElement) {
+        $hours.innerText = formatTime((diff % DAY) / HOUR);
+      }
 
-            if ($seconds instanceof HTMLElement) {
-                $seconds.innerText = formatTime((diff % MINUTE) / SECOND)
-            }
-        }
+      if ($minutes instanceof HTMLElement) {
+        $minutes.innerText = formatTime((diff % HOUR) / MINUTE);
+      }
 
-        const intervalId = setInterval(updateCountdown, SECOND)
-        updateCountdown();
-
-        return () => clearInterval(intervalId)
+      if ($seconds instanceof HTMLElement) {
+        $seconds.innerText = formatTime((diff % MINUTE) / SECOND);
+      }
     }
-    useEffect(() => {
-        init()
-    });
 
-    return (
-        <section className='section-countdown'>
-            <img src="./favicon.svg" alt="Logo de La Velada" className='la-velada'/>
+    const intervalId = setInterval(updateCountdown, SECOND);
+    updateCountdown();
 
-            <h3 className='subtitle'>El tiempo restante para La Velada IV es...</h3>
+    return () => clearInterval(intervalId);
+  }
+  useEffect(() => {
+    init();
+  });
 
-            <div data-date={TIMESTAMP_END} className='contador'>
+  return (
+    <section className="section-countdown">
+      <img src="./favicon.svg" alt="Logo de La Velada" className="la-velada" />
 
-                <div className='date'>
-                    <span className='numbers' data-days>00</span>
-                    <span className='fecha'>Días</span>
-                </div>
+      <h3 className="subtitle">El tiempo restante para La Velada IV es...</h3>
 
-                <span className='points' aria-hidden="true">:</span>
+      <div data-date={TIMESTAMP_END} className="contador">
+        <div className="date">
+          <span className="numbers" data-days>
+            00
+          </span>
+          <span className="fecha">Días</span>
+        </div>
 
-                <div className='date'>
-                    <span className='numbers' data-hours>00</span>
-                    <span className='fecha'>Horas</span>
-                </div>
+        <span className="points" aria-hidden="true">
+          :
+        </span>
 
-                <span className='points' aria-hidden="true">:</span>
+        <div className="date">
+          <span className="numbers" data-hours>
+            00
+          </span>
+          <span className="fecha">Horas</span>
+        </div>
 
-                <div className='date'>
-                    <span className='numbers' data-minutes>00</span>
-                    <span className='fecha'>Minutos</span>
-                </div>
+        <span className="points" aria-hidden="true">
+          :
+        </span>
 
-                <span className='points' aria-hidden="true">:</span>
+        <div className="date">
+          <span className="numbers" data-minutes>
+            00
+          </span>
+          <span className="fecha">Minutos</span>
+        </div>
 
-                <div className='date'>
-                    <span className='numbers' data-seconds>00</span>
-                    <span className='fecha'>Segundos</span>
-                </div>
-            </div>
-        </section>
-    )
-}
+        <span className="points" aria-hidden="true">
+          :
+        </span>
 
+        <div className="date">
+          <span className="numbers" data-seconds>
+            00
+          </span>
+          <span className="fecha">Segundos</span>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Countdown
+export default Countdown;
