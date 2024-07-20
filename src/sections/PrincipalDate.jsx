918c94acad6695c70fb0fe7c26c8f69b998b74c5
Josue Gonzalez <josuegr834@gmail.com>
2024-07-20 12:59:57 -0300

    Modificando los componentes para dar mensaje de finalizacion

--- src/sections/PrincipalDate.jsx
+++ src/sections/PrincipalDate.jsx
@@ -1,23 +1,33 @@
-import './styles/PrincipalDate.css'
+import "./styles/PrincipalDate.css";
 
 const PrincipalDate = () => {
-    return (
-        <section className='section-principaldate'>
-            <h2 className='date-title'>
-                13 de julio <span aria-hidden className='hidden md:inline'> . </span><br aria-hidden className='block md:hidden'/>
-                17:00h CET
-            </h2>
+  return (
+    <section className="section-principaldate">
+      <h2 className="date-title">
+        13 de julio{" "}
+        <span aria-hidden className="hidden md:inline">
+          {" "}
+          .{" "}
+        </span>
+        <br aria-hidden className="block md:hidden" />
+        17:00h CET
+      </h2>
 
-            <h3 className='date-place'>
-                <span>Evento de Estreno</span>
-                <span>Estadio Santiago Bernabéu (Madrid)</span>
-            </h3>
+      <h3 className="date-place">
+        <span>Evento finalizado</span>
+        <span>Estadio Santiago Bernabéu (Madrid)</span>
+      </h3>
 
-            <a href="https://www.entradas.com/artist/la-velada-del-ano/?affiliate=LVL" target='_blank' rel='noopener noreferrer' className='a-principaldate'>
-                Entradas
-            </a>
-        </section>
-    )
-}
+      <a
+        // href="https://www.entradas.com/artist/la-velada-del-ano/?affiliate=LVL"
+        href="javascript:void(0)"
+        rel="noopener noreferrer"
+        className="a-principaldate"
+      >
+        Sin entradas
+      </a>
+    </section>
+  );
+};
 
-export default PrincipalDate
+export default PrincipalDate;
